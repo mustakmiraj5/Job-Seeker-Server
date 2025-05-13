043a6d503d11441892df804415864a4c5a0117d1
mustakmiraj5 <msmiraj8@gmail.com>
2025-05-14 02:21:26 +0600

    update

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hixyzlt.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hixyzlt.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://wildmiraj:CYB8vn352ibsf7Lk@cluster0.hekcm8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
